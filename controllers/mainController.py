3f5a4692722a0515eae118eec3dba18d325c95ec
kionnseur <candaes.yan@gmail.com>
2023-06-06 17:06:18 +0200

    ajout de django, angular, vue, laravel, flask, express

--- controllers/mainController.py
+++ controllers/mainController.py
@@ -10,7 +10,7 @@ fenetre = tk.Tk()
 tfFolder = tk.Entry(fenetre)
 
 # Type
-options = ["symfony", "django", "react"]
+options = ["symfony", "django", "react","angular","vue","laravel","flask","express"]
 lType = tk.Label(text="Please select a type:")
 cbType = ttk.Combobox(fenetre, values=options, state='readonly')
 
@@ -63,13 +63,14 @@ def verifieName():
 def createProject():
     # proj = Projet(tfFolder.get(),cbType.get(),tfName.get())
     projName=tfFolder.get()+"/"+tfName.get()
+    lError.pack(side="bottom")
     try:
         match cbType.get():
             case "react":
-               if os.system("node -v")==0:
-                    os.system(f"npx create-react-app {projName}")
-               else:
+               if os.system("node -v")!=0:
                    lError.config(text="Node.js n'est pas installé. Veuillez le télécharger à partir de https://nodejs.org.")
+               else:
+                   os.system(f"npx create-react-app {projName}")
 
             case "symfony":
                 if os.system("php -v") != 0:
@@ -79,9 +80,70 @@ def createProject():
                 else:
                     os.system(f"composer create-project symfony/website-skeleton {projName}")
 
+            case "django":
+                try:
+                    os.chdir(tfFolder.get())
+                    import django
+                except OSError as e:
+                    lError.config(text=f"Erreur lors de la création du répertoire : {e}")
+                except ImportError:
+                    try:
+                        os.system("pip install Django")
+                    except Exception as e:
+                        lError.config(text="Erreur lors de l'installation de Django : " + str(e))
+                os.system(f"django-admin startproject {tfName.get()}")
+
+            case "angular":
+                os.chdir(tfFolder.get())
+                if os.system("ng version") != 0:
+                    lError.config(text="Installation de Angular CLI.")
+                    os.system("npm install -g @angular/cli")
+                os.system(f"ng new {tfName.get()} --skip-git")
+
+            case "vue":
+                os.chdir(tfFolder.get())
+                if os.system("vue --version") != 0:
+                    lError.config(text="Installation de Vue CLI.")
+                    os.system("npm install -g @vue/cli")
+                os.system(f"vue create {tfName.get()} --no-git --registry=https://registry.npmjs.org --default")#--skip-install pour retirer yarn
+
+            case "laravel":
+                os.chdir(tfFolder.get())
+                if os.system("laravel --version") != 0:
+                    lError.config(text="Installation de Laravel CLI.")
+                elif os.system("composer --version") != 0:
+                    lError.config(text="Composer n'est pas installé. Veuillez le télécharger à partir de https://getcomposer.org.")
+                else:
+                    os.system(f"laravel new {tfName.get()}")
+
+            case "flask":
+                try:
+                    import flask
+                except ImportError:
+                    try:
+                        os.system("pip install Flask")
+                    except Exception as e:
+                        lError.config(text="Erreur lors de l'installation de Flask : " + str(e))
+                else:
+                    lError.config(text="Flask est déjà installé.")
+                os.chdir(tfFolder.get())
+                os.mkdir(tfName.get())
+                os.chdir(tfName.get())
+                with open("app.py", "w") as file:
+                    file.write("from flask import Flask\n\napp = Flask(__name__)\n\n@app.route('/')\ndef index():\n    "
+                               "return 'Hello, Flask!'\n\nif __name__ == '__main__':\n    app.run()")
+
+            case "express":
+                if os.system("express --version") != 0:
+                    lError.config(text="Installation de Express.js.")
+                os.system("npm install -g express-generator")
+                os.system(f"express --view=pug {projName}")
+
             case _:
                 lError.config(text="error no type selected")
     except Exception as e:
         lError.config(text=str(e))
 
-    lError.pack(side="bottom")
+
+    if lError.cget("text")=="":
+        lError.config(text="Projet créé avec succès !")
